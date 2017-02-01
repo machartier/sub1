abc3a12a0d683ffdfcdf2e870f9139d0aa64e46f
Marie Anne Chartier <ma.chartier@gmail.com>
2017-02-01 17:37:26 +0100

    commit after v0.1

---
+++ src/Subclass2.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Subclass1
+ *
+ * @author macha
+ */
+class Subclass1 {
+    public function newMethod(){}
+    public function addMethodAfterTag1(){
+        //fix v1=>0.1
+    }
+
+
+}
